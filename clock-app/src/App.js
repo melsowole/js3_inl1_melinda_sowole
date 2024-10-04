5c0879176d7b2eea0ea2e95ed52e259183cd9904
melsowole <82158719+melsowole@users.noreply.github.com>
2024-10-04 11:19:46 +0200

    useEffect for update time

--- clock-app/src/App.js
+++ clock-app/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Clock from "./Clock.js";
 
 function App() {
   const [time, setTime] = useState(new Date());
 
-  setInterval(() => {
-    setTime(new Date());
-  }, 1000);
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+  }, []);
 
   return (
     <div className="App">
--- clock-app/src/Clock.js
+++ clock-app/src/Clock.js
@@ -1,9 +1,5 @@
 export default function Clock(props) {
   const { time } = props;
 
-  return (
-    <p>
-      {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
-    </p>
-  );
+  return <p>{time.toLocaleTimeString()}</p>;
 }
